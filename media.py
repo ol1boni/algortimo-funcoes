dac1bd03963240d75628a04efeb7535f281365c0
ol1boni <maluoliboni1028@gmail.com>
2026-05-25 21:46:23 -0300

    .

---
+++ media.py
@@ -0,0 +1,6 @@
+def media (a,b,c):
+    return (a + b + c) /3
+n1 = float(input('Digite o primero número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
+print (media(n1,n2,n3))
---
+++ par.py
@@ -0,0 +1,7 @@
+def par (a):
+    if a%2 == 0:
+        return f'{a} é par'
+    else:
+        return f'{a} é ímpar'
+n1 = float(input('Digite um númro para verficar se é par ou ímpar: '))
+print (par(n1))
